1511048db4eb2128fa54e449d7e9fb7dc84794b4
AhmedLabibb <92722943+AhmedLabibb@users.noreply.github.com>
2022-02-18 21:13:17 +0400

    Initial commit

---
+++ src/main/java/com/bsf/bsfservice/BsfServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bsf.bsfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsfServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsf/bsfservice/BsfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsf.bsfservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
